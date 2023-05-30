d5f0c7c6865cf1213e5da9734c424fd4de3f9ea6
Francisco Vieira <fvieira@c1r15s1.42porto.com>
2023-05-30 22:18:50 +0100

    started ex00

---
+++ ex00/Animal.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 21:58:22 by fvieira           #+#    #+#             */
+/*   Updated: 2023/05/30 21:58:24 by fvieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+protected:
+	std::string type;
+public:
+	Animal();
+	~Animal();
+	void	makeSound(void);
+	
+};
+#endif
